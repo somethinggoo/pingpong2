3cd59f736ac9d0b1e98ff4c7f694be656cb09bb1
beyblade <87214014+somethinggoo@users.noreply.github.com>
2022-04-17 12:43:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function preload() {
+    ball_touch_paddel = loadSound("");
+    missed = loadSound("");
+  }
+  
+  function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  }
